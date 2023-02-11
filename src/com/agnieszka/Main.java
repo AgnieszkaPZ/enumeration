e9d948fd038824b775e3d91c92b49bda3906e39d
AgnieszkaPZ <paulewicz.aga@gmail.com>
2023-02-11 22:46:06 +0100

    Added code to Main method

--- src/com/agnieszka/Main.java
+++ src/com/agnieszka/Main.java
@@ -9,9 +9,19 @@ public class Main {
         DaysOfTheWeek weekDay = DaysOfTheWeek.TUES;
         System.out.println(weekDay);
 
-        System.out.printf("Name is %s", weekDay.name());
-        System.out.println();
-        System.out.printf("Order number = %d%n", weekDay.ordinal());
+        //System.out.println();
+        //System.out.printf("Order number = %d%n", weekDay.ordinal());
+
+        for (int i =0 ; i < 10; i++){
+            weekDay = randomDay();
+            System.out.printf("Name is %s, Order number = %d%n", weekDay.name(), weekDay.ordinal());
+
+            if(weekDay == weekDay.FRI){
+                System.out.println("Friday found !!!");
+            }
+
+        }
+
     }
 
     public static DaysOfTheWeek randomDay(){
